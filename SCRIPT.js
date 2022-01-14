377093aecaa42b502c4828ee20ff6f09a6b2ddd0
renangrocha <renan.grocha@hotmail.com>
2022-01-14 17:54:44 -0300

    aula 4

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,1 +1,21 @@
 const imagem = document.querySelector('img');
+const botao = document.querySelector('button')
+const nomeDoPersonagem = document.querySelector ('#nome')
+const especie = document.querySelector('#especie')
+const status = document.querySelector('#statos')
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json', 
+            "Content-type": 'applications/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+    });
+}
+
+botao.onclick = pegarPersonagem;
